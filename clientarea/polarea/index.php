2d4b4a61d311991f530532b4ab3d4fd51675c4e0
HerrEisbaer <herreisbaer.business@gmail.com>
2022-06-19 11:26:29 +0200

    suiiii

---
+++ clientarea/polarea/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="../../../css/clientarea/polarea/leitstelle/index.css"/>
+    <title>Leistelle</title>
+  </head>
+  <body>
+    <a href="logout.php">Abmelden</a>
+    <a href="leitstelle/">Leitstelle</a>
+      <?php
+
+      session_start();
+      if(!isset($_SESSION["username"])){
+        header("Location: login.php");
+        exit;
+      }
+
+      $username = $_SESSION["username"];
+
+      print($username)
+     ?>
+    </div>
+  </body>
+</html>
--- js/index.js
+++ js/index.js
@@ -1,120 +1,43 @@
-const linksbutton = document.getElementById("linkbutton");
-const linkcontainer = document.getElementById("linkcontainer");
-const testlinks = document.getElementById("testlinks");
-const allglinks = document.getElementById("allglinks");
-const linkcontainerh = document.getElementById("linkcontainerh")
+const überschrift = document.getElementsByClassName("überschrift")
 
-var bruh = false
+for(var i = 0; i < überschrift.length; i++) {
+    (function(index) {
+        überschrift[index].addEventListener("click", function() {
+         weh()
+       })
+    })(i);
+}
 
-linksbutton.addEventListener('click', function () {
-    if (linksbutton.innerHTML == "Einklappen") {
-        linksbutton.classList.add("hidden")
-        linksbutton.innerHTML = "Ausklappen"
-        slidemiddleleft(testlinks)
-        slidemiddleright(allglinks)
-        delay(1).then( () => linkcontainer.classList.add("slideup")).then( () => linkcontainerh.classList.add("slideup")).then( 
-            () => delay(1).then( () => linkcontainer.classList.add("hidden")).then( () => linkcontainerh.classList.add("hidden")).then( 
-            () => resettransform(testlinks)).then( () => linkcontainerh.classList.remove("slideup")).then( 
-            () => linkcontainer.classList.remove("slideup")).then( () => resetslides(testlinks)).then( () => resetslides(allglinks)).then( () => linksbutton.classList.remove("hidden")))
-        
-    } else if (linksbutton.innerHTML == "Ausklappen") {
-        linksbutton.innerHTML = "Einklappen"
-        linkcontainer.classList.remove("hidden")
-        linkcontainerh.classList.remove("hidden")
-    }
+const unterschrift = document.getElementsByClassName("klickmich")
+
+for(var i = 0; i < unterschrift.length; i++) {
+    (function(index) {
+        unterschrift[index].addEventListener("click", function() {
+         weh()
+       })
+    })(i);
+}
+
+document.getElementById("blauesteilzuwerbung").addEventListener('click', () => {
+    weh2()
 })
 
-function changebutton(button) {
-    if (button.innerHTML == "Einklappen") {
-        button.innerHTML = "Ausklappen"
-    } else if (button.innerHTML == "Ausklappen") {
-        button.innerHTML = "Einklappen"
-    }
-}
-
-function slidemiddleleft(element) {
-    element.classList.add("slideleft")
-    delay(10).then( () => element.classList.remove("slideleft"))
-}
-
-function slidemiddleright(element) {
-    element.classList.add("slideright")
-    delay(10).then( () => element.classList.remove("slideright"))
-}
-
-function resetslides(element) {
-    element.classList.remove("slideleft")
-    element.classList.remove("slideright")
-} 
-
-function resettransform(element) {
-    element.classList.add("notransform")
-    delay(0.1).then( () => element.classList.remove("notransform"))
-}
-
-function delay(time) {
-    time = time * 1000;
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
-
-let users = []
-
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.personummer.includes(value) ||
-      user.haartyp.toLowerCase().includes(value) ||
-      user.haarfarbe.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
-  })
+document.getElementById("blauesteilzubilder").addEventListener('click', () => {
+    weh3()
 })
 
-fetch("menschen.json")
-  .then(res => res.json())
-  .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      const haartyp = card.querySelector("[data-haartyp]")
-      const haarfarbe = card.querySelector("[data-haarfarbe]")
-      const image = card.querySelector("[data-image]")
-      header.textContent = user.name
-      body.textContent = user.personummer
-      haartyp.textContent = user.haartyp
-      haarfarbe.textContent = user.haarfarbe
-      image.src = user.gesichtbild
-      userCardContainer.append(card)
-      return { name: user.name, personummer: user.personummer, haartyp: user.haartyp, haarfarbe: user.haarfarbe, element: card }
-    })
+document.addEventListener('keydown', (event) => {
+    if (event.code === "Space") weh()
 })
 
-const atagstest = document.getElementsByClassName("atest")
-
-for(var i = 0; i < atagstest.length; i++) {
-  (function(index) {
-    atagstest[index].addEventListener("click", function() {
-       navigator.clipboard.writeText(atagstest[index].href);
-     })
-  })(i);
+function weh() {
+    window.location.href = "clientarea"
 }
 
-const img = document.getElementById('logo')
-var counter = 0;
+function weh2() {
+    window.location.href = "clientarea/werbung"
+}
 
-img.addEventListener("click", ev => {
-    if(counter < 3) {
-        counter++
-        return;
-    }
-    if(counter >= 3) {
-        counter = 0;
-        window.location.href
-    }
-})
+function weh3() {
+    window.location.href = "clientarea/bilder"
+}
